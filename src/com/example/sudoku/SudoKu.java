0e2939bb7776af2cf5defab5c3ab0175e5ab2e6c
laizhihuan <laizhihuan@basecity.com>
2014-03-11 09:17:51 +0800

    add about dialog

--- src/com/example/sudoku/SudoKu.java
+++ src/com/example/sudoku/SudoKu.java
@@ -2,14 +2,31 @@ package com.example.sudoku;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class SudoKu extends Activity {
+public class SudoKu extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		//Set up click listener for all the buttons
+		View continueButton = findViewById(R.id.continue_button);
+		continueButton.setOnClickListener(this);
+		
+		View newGameButton = findViewById(R.id.new_button);
+		newGameButton.setOnClickListener(this);
+		
+		View aboutButton = findViewById(R.id.about_button);
+		aboutButton.setOnClickListener(this);
+		
+		View exitButton = findViewById(R.id.exit_button);
+		exitButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -19,4 +36,15 @@ public class SudoKu extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.about_button:
+			Intent intent = new Intent(this,About.class);
+			startActivity(intent);
+			break;
+		}
+	}
+	
+	
 }
